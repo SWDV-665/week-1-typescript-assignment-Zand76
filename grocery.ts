39c3b048a84f330801250f1176589e0dd257ee62
Zand76 <37522349+Zand76@users.noreply.github.com>
2021-10-24 18:30:50 -0400

    Update grocery.ts

--- grocery.ts
+++ grocery.ts
@@ -1,28 +1,31 @@
 //Create grocery class
 class Grocery {
-    name:string;
-    price:number;
-    quantity:number;
+    name: string;
+    price: number;
+    quantity: number;
 
-    constructor(name:string, price:number, quantity:number){
+    constructor(name: string, price: number, quantity: number){
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
+}
 
-    displayGroceryItem(): void {
-        console.log('This grocery item is: ${this.name}');
-    }
+interface Item {
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+function showList(item: Item) {
+    return "Item Details: " + Item.name + " " + Item.price + " " + Item.quantity;
 }
 
 //Create grocery items array
-const Milk = new Grocery('Milk', 4.99, 1);
-Milk.displayGroceryItem();
-
 let grocery_list = [
     new Grocery('Milk', 1, 3),
     new Grocery('Bread', 2, 6),
     new Grocery('Eggs', 3, 11)
-]
+];
 
-
+document.body.textContent = showList(grocery_list);

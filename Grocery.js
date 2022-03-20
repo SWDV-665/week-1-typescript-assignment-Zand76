fa7bcb14b2787f37bd2a7c8591364b9a1249251f
Zand76 <37522349+Zand76@users.noreply.github.com>
2022-03-19 20:04:50 -0400

    Rework WK1 Assignment

--- Grocery.js
+++ Grocery.js
@@ -1,10 +1,22 @@
+var Grocery = /** @class */ (function () {
+    function Grocery(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    return Grocery;
+}());
 //Declaire Array
-var groceries = ['Milk', 'Bread', 'Eggs'];
+var groceries = [
+    new Grocery('Milk', 1.99, 1),
+    new Grocery('Bread', 2.99, 2),
+    new Grocery('Eggs', 3.99, 6)
+];
 //DOM Interaction
-var groceryList = document.getElementById('GroceryList');
+var ele = document.getElementById('GroceryList');
 //List items
-groceries.forEach(function (item) {
+groceries.forEach(function (e) {
     var li = document.createElement('li');
-    li.innerText = item;
-    groceryList.appendChild(li);
+    li.textContent = "".concat(e.quantity, " ").concat(e.name, " ").concat(e.price);
+    ele.appendChild(li);
 });
--- Grocery.ts
+++ Grocery.ts
@@ -1,12 +1,28 @@
+class Grocery {
+    name: string;
+    price: number;
+    quantity: number;
+
+    constructor(name:string, price:number, quantity:number) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
 //Declaire Array
-let groceries = ['Milk', 'Bread', 'Eggs'];
+let groceries = [
+    new Grocery ('Milk', 1.99, 1), 
+    new Grocery ('Bread', 2.99, 2), 
+    new Grocery ('Eggs', 3.99, 6)
+]
 
 //DOM Interaction
-let groceryList = document.getElementById('GroceryList');
+const ele = document.getElementById('GroceryList');
 
 //List items
-groceries.forEach((item) => {
-    let li = document.createElement('li');
-    li.innerText = item;
-    groceryList.appendChild(li);
+groceries.forEach(e => {
+    const li = document.createElement('li');
+    li.textContent = `${e.quantity} ${e.name} ${e.price}`;
+    ele.appendChild(li);
 });
